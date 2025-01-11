b0370f12237eb50fdce8b5c8486985bf4caf4c7f
WilliamsEnabulele <enabulelewilliamss@gmail.com>
2025-01-11 12:22:18 +0100

    implement recurring payment service apis

---
+++ MonnifyDotnet.SDK/Implementations/RecurringPaymentService.cs
@@ -0,0 +1,27 @@
+﻿using MonnifyDotnet.SDK.Interfaces;
+using MonnifyDotnet.SDK.Models;
+using MonnifyDotnet.SDK.Shared;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+
+namespace MonnifyDotnet.SDK.Implementations
+{
+    public class RecurringPaymentService(BaseService baseService) : IRecurringPaymentService
+    {
+        private readonly BaseService _baseService = baseService;
+
+        /// <summary>
+        /// This endpoint allows you to charge an already tokenized card with it’s card token. **NOTE**: The customer email address used in the first successful charge should be stored along with the card token.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public async Task<BaseResponse> ChargeCardToken(ChargeCardTokenRequest request)
+        {
+            var client = await _baseService.GetAuthenticatedClientAsync();
+            var response = await client.PostAsJsonAsync(RecurringPaymentUrls.ChargeCardToken, request);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<BaseResponse>(content);
+        }
+    }
+}
---
+++ MonnifyDotnet.SDK/Interfaces/IRecurringPaymentService.cs
@@ -0,0 +1,9 @@
+﻿using MonnifyDotnet.SDK.Models;
+
+namespace MonnifyDotnet.SDK.Interfaces
+{
+    public interface IRecurringPaymentService
+    {
+        Task<BaseResponse> ChargeCardToken(ChargeCardTokenRequest request);
+    }
+}
