906cc00cc5173dd801ea5488dce7f325ce96fbae
WilliamsEnabulele <enabulelewilliamss@gmail.com>
2025-01-11 12:22:19 +0100

    add monnify client dependency injection extension

---
+++ MonnifyDotnet.SDK/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MonnifyDotnet.SDK.Implementations;
+using MonnifyDotnet.SDK.Interfaces;
+using MonnifyDotnet.SDK.Models;
+
+namespace MonnifyDotnet.SDK.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddMonnify(this IServiceCollection services, Action<MonnifyOptions> options)
+        {
+            services.Configure(options);
+            services.AddHttpClient("MonnifyClient", client =>
+            {
+                client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+            });
+
+            services.AddScoped<IMonnifyClient, MonnifyClient>();
+            return services;
+        }
+    }
+}
