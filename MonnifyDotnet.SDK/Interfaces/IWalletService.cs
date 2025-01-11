1d0cd674b89741a1d0f1cb13f56fbbf14533d602
WilliamsEnabulele <enabulelewilliamss@gmail.com>
2025-01-11 12:22:19 +0100

    implement wallet service apis

---
+++ MonnifyDotnet.SDK/Interfaces/IWalletService.cs
@@ -0,0 +1,15 @@
+﻿using MonnifyDotnet.SDK.Models;
+
+namespace MonnifyDotnet.SDK.Interfaces
+{
+    public interface IWalletService
+    {
+        Task<BaseResponse<CreateWalletResponse>> CreateWallet(CreateWalletRequest request);
+
+        Task<BaseResponse<WalletBalanceResponse>> GetWalletBalance(string accountNumber);
+
+        Task<BaseResponse<Wallet>> GetWallets(GetWalletsFilterRequest request);
+
+        Task<BaseResponse<WalletTransactionsResponse>> GetWalletTransactions(string accountNumber);
+    }
+}
