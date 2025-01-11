f7f814b45a52f5d378e28741c86ff4aa65833e6a
WilliamsEnabulele <enabulelewilliamss@gmail.com>
2025-01-11 12:22:18 +0100

    implement other service apis

---
+++ MonnifyDotnet.SDK/Implementations/OtherService.cs
@@ -0,0 +1,30 @@
+﻿using MonnifyDotnet.SDK.Interfaces;
+using MonnifyDotnet.SDK.Models;
+using MonnifyDotnet.SDK.Shared;
+using Newtonsoft.Json;
+
+namespace MonnifyDotnet.SDK.Implementations
+{
+    public class OtherService(BaseService baseService) : IOtherService
+    {
+        private readonly BaseService _baseService = baseService;
+
+        public async Task<BaseResponse<BankResponse[]>> GetBanks()
+        {
+            var client = await _baseService.GetAuthenticatedClientAsync();
+            var response = await client.GetAsync(OtherUrls.GetBanks);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<BaseResponse<BankResponse[]>>(content);
+        }
+
+        public async Task<BaseResponse<BankResponse[]>> GetBanksWithUSSDShortCode()
+        {
+            var client = await _baseService.GetAuthenticatedClientAsync();
+            var response = await client.GetAsync(OtherUrls.GetBanksWithUSSDShortCode);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<BaseResponse<BankResponse[]>>(content);
+        }
+    }
+}
---
+++ MonnifyDotnet.SDK/Interfaces/IOtherService.cs
@@ -0,0 +1,11 @@
+﻿using MonnifyDotnet.SDK.Models;
+
+namespace MonnifyDotnet.SDK.Interfaces
+{
+    public interface IOtherService
+    {
+        Task<BaseResponse<BankResponse[]>> GetBanks();
+
+        Task<BaseResponse<BankResponse[]>> GetBanksWithUSSDShortCode();
+    }
+}
